9d8c977f73c20f062b025037af5bd76a98813ff5
C4cte <cacte.pro@gmail.com>
2025-05-25 14:50:56 +0200

    Update script.js
    Day 1 (2025/05/07)

--- script.js
+++ script.js
@@ -12,36 +12,26 @@ const results = {
     "Frisk": { "darkdust": "1-0" },
     "darkdust": { "Grégory": "1-0", "Frisk": "0-1", "SezTy": "1-0", "darkness": "1-0", "penguin": "1-0", "Nevender": "1-0", "pointriangle": "0-1", "Rip_toi": "0-1" },
     "SezTy": { "darkdust": "0-1", "darkness": "0-1", "penguin": "1-0", "444_ma..li": "1-0", "Nevender": "0-1", "pointriangle": "0-1", "Rip_toi": "1-0", "cripie333": "1-0" },
-    "darkness": { "Grégory": "1-0", "darkdust": "0-1", "SezTy": "1-0", "Nevender": "1-0", "Draco": "1-0", "pointriangle": "0-1", "Fyn": "1-0"  },
+    "darkness": { "Grégory": "1-0", "darkdust": "0-1", "SezTy": "1-0", "Nevender": "1-0", "Draco": "1-0", "pointriangle": "0-1", "Fyn": "1-0" },
     "Bendospeed9": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" },
     "penguin": { "darkdust": "0-1", "SezTy": "0-1", "Bendospeed9": "1-0", "444_ma..li": "1-0", "Nevender": "0-1", "pointriangle": "1-0", "Fyn": "1-0" },
-    "444_ma..li": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" },
-    "Nevender": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" },
-    "Draco": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" },
-    "Mimo": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" },
-    "pointriangle": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" },
-    "quentin59279": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" },
-    "yoyo1406_": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" },
-    "Fyn": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" },
-    "Rip_toi": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" },
-    "cripie333": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" },
-    "champion93300": { "penguin": "0-1", "pointriangle": "0-1", "quentin59279": "0-1" }
+    "444_ma..li": { "SezTy": "0-1", "penguin": "0-1", "Nevender": "0-1", "pointriangle": "0-1", "yoyo1406_": "N/A", "Rip_toi": "1-0" },
+    "Nevender": { "darkdust": "0-1", "SezTy": "1-0", "darkness": "0-1", "penguin": "0-1", "444_ma..li": "0-1", "Draco": "0-1", "pointriangle": "0-1", "cripie333": "1-0", "champion93300": "1-0" },
+    "Draco": { "darkness": "0-1", "Nevender": "0-1", "Mimo": "1-0", "pointriangle": "1-0", "Fyn": "1-0" },
+    "Mimo": { "Draco": "0-1" },
+    "pointriangle": { "darkdust": "0-1", "SezTy": "0-1", "darkness": "0-1", "Bendospeed9": "1-0", "penguin": "0-1", "444_ma..li": "1-0", "Nevender": "1-0", "Draco": "1-0", "quentin59279": "1-0" },
+    "quentin59279": { "Bendospeed9": "1-0", "pointriangle": "0-1" },
+    "yoyo1406_": { "444_ma..li": "N/A" },
+    "Fyn": { "darkness": "0-1", "penguin": "0-1", "Draco": "0-1" },
+    "Rip_toi": { "darkdust": "0-1", "SezTy": "0-1", "444_ma..li": "0-1", "cripie333": "½-½" },
+    "cripie333": { "Grégory": "0-1", "SezTy": "0-1", "Nevender": "0-1", "Rip_toi": "½-½" },
+    "champion93300": { "Nevender": "0-1" }
   },
   // Jour 2
   2: {
-    "Alice": { "Bob": "1-0", "Charlie": "1-0", "Diana": "0-1", "Eve": "½–½" },
-    "Bob": { "Alice": "0-1", "Charlie": "1-0", "Diana": "1-0", "Eve": "½–½" },
-    "Charlie": { "Alice": "0-1", "Bob": "0-1", "Diana": "1-0", "Eve": "1-0" },
-    "Diana": { "Alice": "1-0", "Bob": "0-1", "Charlie": "0-1", "Eve": "0-1" },
-    "Eve": { "Alice": "½–½", "Bob": "½–½", "Charlie": "0-1", "Diana": "1-0" }
   },
   // Jour 3
   3: {
-    "Alice": { "Bob": "½–½", "Charlie": "1-0", "Diana": "1-0", "Eve": "0-1" },
-    "Bob": { "Alice": "½–½", "Charlie": "1-0", "Diana": "1-0", "Eve": "0-1" },
-    "Charlie": { "Alice": "0-1", "Bob": "0-1", "Diana": "1-0", "Eve": "1-0" },
-    "Diana": { "Alice": "0-1", "Bob": "0-1", "Charlie": "0-1", "Eve": "1-0" },
-    "Eve": { "Alice": "1-0", "Bob": "1-0", "Charlie": "0-1", "Diana": "0-1" }
   }
 };
 
